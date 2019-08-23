ba340168c084a26231c335f9856f46fe5ced2e02
vbvijaybang <vbvijaybang@gmail.com>
2019-08-24 01:30:14 +0530

    Added test file with package.json

---
+++ test.js
@@ -0,0 +1,37 @@
+const KinesisProducer = require("aws-kinesis-producer");
+
+
+const options = {
+    streamName: "test-stream",
+    schema: null,
+    retryInterval: 1500,
+    kinesisClient: null,
+    maxDrains: 3,
+    maxRetries: 10,
+    maxRecords: 1,
+    maxTime: 0,
+    chunkSize: 400,
+    kinesisOptions: {
+        region: 'aws region',
+        accessKeyId: "Your access key",
+        secretAccessKey: "Your secret access key"
+    },
+};
+
+
+let kinesisProducer = new KinesisProducer(options);
+let record = {
+    "message": "Test is test data"
+}
+
+kinesisProducer.on('drainError', (errors) => {
+    console.log(`error: ${JSON.stringify(errors)}`)
+});
+
+kinesisProducer.on('drainSuccess', (records) => {
+    console.log(`success records: ${records.length}`)
+});
+
+for(let i =0; i < 1; i++) {
+    kinesisProducer.putRecords([record]);
+}
